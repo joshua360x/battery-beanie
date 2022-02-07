5fe2e93c54226449327a08e02ae9a0dadbfad283
joshua360x <jkw5499@psu.edu>
2022-02-07 15:55:02 -0500

    rendered babies to page from db

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
+import './App.css';
+import { getBeanieBabies } from './services/fetch-utils';
+
+async function beanies() {
+  const beanieBabies = await getBeanieBabies();
+  return beanieBabies;
+}
 
 export default function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>Search</li>
+          </ul>
+        </nav>
         <Switch>
-          <Route>
+          <Route exact path="/">
             {/* this home page route should list all the beanies */}
+            <BeaniesPage />
           </Route>
-          <Route> 
+          <Route path="/beanie-babies/:id">
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
+
           </Route>
         </Switch>
       </div>
--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
+    <div className='beanie'>
+      <Link>{beanieBaby.title}</Link>
+      <img src={beanieBaby.image} alt="" />
+    </div>
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    // null
   );
 }
