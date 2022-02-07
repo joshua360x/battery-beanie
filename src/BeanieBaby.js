bed671791802885782a098cb744376091b5a7268
joshua360x <jkw5499@psu.edu>
2022-02-07 17:43:28 -0500

    linked beanie baby detail by id

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
-    <div className='beanie'>
-      <Link>{beanieBaby.title}</Link>
-      <img src={beanieBaby.image} alt="" />
-    </div>
+    <Link to={`./beanie-babies/${beanieBaby.id}`}>
+      <div className="beanie">
+        <p>{beanieBaby.title}</p>
+        <img src={beanieBaby.image} alt="" />
+      </div>
+    </Link>
     // it should also render the beanie baby's image and show the beanie baby's name
     // null
   );
