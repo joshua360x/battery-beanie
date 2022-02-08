dac25946ac44ecee25e3924ce9d369a5229f6f52
joshua360x <jkw5499@psu.edu>
2022-02-07 20:11:42 -0500

    dropdown menu rendered

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -7,11 +7,12 @@ function App() {
   const [beanieBabies, setBeanieBabies] = useState([]);
   const [page, setPage] = useState(1);
   const [allBennies, setAllBennies] = useState(0);
-  const perPage = 500;
+  const [perPage, setPerPage] = useState(30);
+  // const perPage = 500;
 
   function maxNumber(page, allBeanies, perPage) {
     // const allBeanies = await getALLBeanieBabies();
-    const calc = allBeanies / perPage;
+    const calc = Math.floor(allBeanies / perPage);
     console.log('🚀 ~ file: BeaniesPage.js ~ line 14 ~ maxNumber ~ calc', calc);
     if (page >= calc) {
       console.log('is it true?');
@@ -41,11 +42,17 @@ function App() {
     }
 
     fetch();
-  }, [page, allBennies]); // what can you do with this array to trigger a fetch every time the page changes?
+  }, [page, allBennies, perPage]); // what can you do with this array to trigger a fetch every time the page changes?
 
   return (
     <>
       <h2>Current Page {page}</h2>
+      <select onChange={(e) => setPerPage(e.target.value)}>
+        <option defaultValue="30" value="30">30</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+        <option value={`${allBennies}`}>Show Me Everything</option>
+      </select>
       <div className="buttons">
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
@@ -53,7 +60,10 @@ function App() {
           Previous Page
         </button>
         {/* on click, this button should increment the page in state  */}
-        <button disabled={maxNumber(page, allBennies, perPage)} onClick={() => setPage(page + 1)}>
+        {/* <button disabled={maxNumber(page, allBennies, perPage)} onClick={() => setPage(page + 1)}>
+          Next Page
+        </button> */}
+        <button disabled={page >= allBennies / perPage} onClick={() => setPage(page + 1)}>
           Next Page
         </button>
       </div>
